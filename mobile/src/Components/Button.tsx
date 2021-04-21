a2aef34b2ce673a836f43665d5acdcfa38b5a623
Josimar16 <josimarjr479@gmail.com>
2021-04-21 10:10:54 -0300

    Criando componente de botão

--- mobile/src/Components/Button.tsx
+++ mobile/src/Components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
@@ -9,12 +10,12 @@ interface ButtonProps extends TouchableOpacityProps {
 export function Button({ title, ...rest }: ButtonProps) {
   return (
     <TouchableOpacity
-      style={styles.button}
+      style={styles.container}
       activeOpacity={0.7}
       {...rest}
     >
       <Text
-        style={styles.buttonText}
+        style={styles.text}
       >
         {title}
       </Text>
@@ -23,18 +24,16 @@ export function Button({ title, ...rest }: ButtonProps) {
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 16,
-    marginBottom: 10,
     height: 56,
-    width: 56,
-    paddingHorizontal: 10
   },
-  buttonText: {
+  text: {
     color: colors.white,
-    fontSize: 24
+    fontSize: 16,
+    fontFamily: fonts.heading
   },
 });
