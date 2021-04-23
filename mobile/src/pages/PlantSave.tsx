9fc4b7bcd85dbf935b77e01ba49b424f6029daca
Josimar16 <josimarjr479@gmail.com>
2021-04-22 23:17:34 -0300

    feat: :sparkles: Criado page de minhas plntas

--- mobile/src/pages/PlantSave.tsx
+++ mobile/src/pages/PlantSave.tsx
@@ -11,7 +11,7 @@ import {
 } from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
-import { useRoute } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 import { format, isBefore } from 'date-fns';
 
@@ -21,7 +21,7 @@ import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-import { IPlantProps, savePlant, loadPlant } from '../libs/storage';
+import { IPlantProps, savePlant } from '../libs/storage';
 
 interface IParams {
   plant: IPlantProps
@@ -31,6 +31,7 @@ export const PlantSave = () => {
   const [selectedDateTime, setSelectedDateTime] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(Platform.OS == 'ios');
 
+  const navigation = useNavigation();
   const route = useRoute();
 
   const { plant } = route.params as IParams;
@@ -57,6 +58,14 @@ export const PlantSave = () => {
       await savePlant({
         ...plant,
         dateTimeNotification: selectedDateTime
+      });
+
+      navigation.navigate('Confirmation', {
+        title: 'Tudo certo',
+        subtitle: 'Fique tranquilo que sempre vamos lembrar você de cuidar da sua plantinha com muito cuidado.',
+        buttonTitle: 'Muito Obrigado :D',
+        icon: 'hug',
+        nextScreen: 'MyPlants'
       });
     } catch {
       Alert.alert('Não foi possível salvar a planta. 😢');
@@ -118,7 +127,7 @@ export const PlantSave = () => {
         }
         <Button
           title="Cadastrar planta"
-          onPress={() => handleSave}
+          onPress={handleSave}
         />
       </View>
     </View>
