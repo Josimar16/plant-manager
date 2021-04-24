f692cd3d3bf6bea75fa07cec1bd26ff2eae8b525
Josimar16 <josimarjr479@gmail.com>
2021-04-23 22:12:46 -0300

    feat: :sparkles: Implementando push notification
    Usando uma lib do expo: expo-notification para realizar os agendamentos

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
-import { Routes } from './src/routes';
+// import * as Notifications from 'expo-notifications';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
 
+import { Routes } from './src/routes';
+import { IPlantProps } from './src/libs/storage';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  // useEffect(() => {
+  // Ouvi todas as notificações
+  // const subscription = Notifications.addNotificationReceivedListener(
+  //   async notification => {
+  //     const data = notification.request.content.data.plant as IPlantProps;
+  //   }
+  // );
+  // return () => subscription.remove();
+
+  // async function notifications() {
+  // Listar todas as notificações
+  // const data = await Notifications.getAllScheduledNotificationsAsync();
+
+  // Cancelar todas as notificações
+  // await Notifications.cancelAllScheduledNotificationsAsync();
+  //}
+  // notifications();
+  // }, []);
 
   if (!fontsLoaded)
     return <AppLoading />;
